e51f83ea88fdbda473ee4056d679a3ffd85796d5
Dawit <dawithayl1994@gmail.com>
2021-09-07 13:09:47 +0300

    New Settings for ALLOWED HOSTS

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'aa0841rke4st#@=v75v+!ys!gudktk!(7ewny)97ht)n8&e%!_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
